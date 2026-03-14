309656731e12ad872e2087a2ec4f525934ea97ea
Dvega7z <vega.dj7@gmail.com>
2026-03-13 23:43:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,25 +11,18 @@ car_data = pd.read_csv('vehicles_us.csv')
 st.title("Venta de Autos Online")
 
 # Descripcion
-st.write("En esta pagina podas encontar el vehiculo que mejor se adapte a tus necesidades")
+st.write("En esta pagina podras encontar el vehiculo que mejor se adapte a tus necesidades")
 
 # Boton para crear histograma
 hist_button = st.button('Construir histograma')
 
-# Lógica a ejecutar cuando se hace clic en el botón
 if hist_button:
-    # Escribir un mensaje en la aplicación
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-
-    # Crear un histograma utilizando plotly.graph_objects
-    # Se crea una figura vacía y luego se añade un rastro de histograma
-    fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
-
-    # Opcional: Puedes añadir un título al gráfico si lo deseas
-    fig.update_layout(title_text='Distribución del Odómetro')
-
-    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
-    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.write('Histograma para ver qué años de vehículos son más comunes')
+    
+    # Crear histograma con model_year
+    fig = px.histogram(car_data, x="model_year")
+    fig.update_layout(title_text='Distribución de Años de Vehículos')
+    
     st.plotly_chart(fig, use_container_width=True)
 
 
@@ -43,5 +36,6 @@ if disp_button:
     fig_scatter = px.scatter(car_data, x="model_year", y="price", 
                             title="Relación entre Año del Modelo y Precio")
     
-    # Mostrar el gráfico
+
     st.plotly_chart(fig_scatter, use_container_width=True)
+    
